bfafa508977b130ce49ecb7c36c3d5805f27ccbc
Gibbs <taihy@hanmail.net>
2017-05-11 16:09:37 +0800

    20170511 Empty Project

---
+++ Data_Validation/Data_Validation.h
@@ -0,0 +1,32 @@
+
+// Data_Validation.h : PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CData_ValidationApp:
+// 이 클래스의 구현에 대해서는 Data_Validation.cpp을 참조하십시오.
+//
+
+class CData_ValidationApp : public CWinApp
+{
+public:
+	CData_ValidationApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CData_ValidationApp theApp;
